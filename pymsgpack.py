f2283f26b6b8c4bb32828cae2cfbf8bf83e0712a
G21 <G21@G21>
2017-09-25 22:03:00 +0900

    implements nil, false, true

--- pymsgpack.py
+++ pymsgpack.py
@@ -1,8 +1,35 @@
 import struct
 
+
 NIL=b'\xC0'
+
+FALSE=b'\xC2'
+TRUE=b'\xC3'
+
 
 def pack(obj):
     if obj==None:
         return NIL
+    elif obj==False:
+        return FALSE
+    elif obj==True:
+        return TRUE
+    else:
+        raise RuntimeException()
+
+
+class Parser:
+    def __init__(self, bytedata):
+        self.bytedata=bytedata
+
+    def is_nil(self):
+        return self.bytedata[0:1]==NIL
+
+    def get_bool(self):
+        if self.bytedata[0:1]==FALSE:
+            return False
+        elif self.bytedata[0:1]==TRUE:
+            return True
+        else:
+            raise RuntimeException()
 
--- tests/basic_test.py
+++ tests/basic_test.py
@@ -6,10 +6,31 @@ import struct
 class TestPyMsgPack(unittest.TestCase):
 
     def test_nil(self):
+        # pack
         packed=pymsgpack.pack(None)
         self.assertEquals(struct.pack('B', 0xc0), packed)
+        # parse
+        parsed=pymsgpack.Parser(packed)
+        self.assertTrue(parsed.is_nil())
+
+    def test_false(self):
+        # pack
+        packed=pymsgpack.pack(False)
+        self.assertEquals(struct.pack('B', 0xc2), packed)
+        # parse
+        parsed=pymsgpack.Parser(packed)
+        self.assertFalse(parsed.get_bool())
+
+    def test_true(self):
+        # pack
+        packed=pymsgpack.pack(True)
+        self.assertEquals(struct.pack('B', 0xc3), packed)
+        # parse
+        parsed=pymsgpack.Parser(packed)
+        self.assertTrue(parsed.get_bool())
 
 
 if __name__ == '__main__':
+    print(type(True))
     unittest.main()
 
